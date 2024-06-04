4d9b0966b1825490732f6ab851ae84666d541664
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-06-03 23:17:57 -0400

    Update AssignmentControlButton.tsx

--- src/Kanbas/Courses/Assignments/AssignmentControlButton.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControlButton.tsx
@@ -1,6 +1,5 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import { BsPlus } from "react-icons/bs";
-import GreenCheckmark from "./GreenCheckmark";
 import "./styles.css"
 export default function AssignmentControlButton() {
   return (
