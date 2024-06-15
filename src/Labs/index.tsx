65ded57f78656388b8a9b1cd70445009aa4b45d6
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-06-14 20:58:47 -0400

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Manasa Pavushetty</h1>
+      <h1>Welcome Ishan</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
