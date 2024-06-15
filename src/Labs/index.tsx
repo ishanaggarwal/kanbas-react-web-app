e79c64ece33e3500fe458e1fe14d015c8d1792d6
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-06-14 20:54:22 -0400

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,14 +6,14 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Welcome Ishan Aggarwal</h1>
+      <h1>Manasa Pavushetty</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
+      <Route path="/" element={<Navigate to="Lab3/*" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Lab3/*" element={<Lab3 />} />
       </Routes>
     </div>
   );
