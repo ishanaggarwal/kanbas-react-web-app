8f00ef273769ba98df4a34a78ed7db3851583f11
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-06-29 12:09:14 -0400

    Update index.tsx

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,4 @@
 import CoursesNavigation from "./Navigation";
-import { courses } from "../Database";
 import { Navigate,Route, Routes, useParams,useLocation  } from "react-router";
 import Modules from "./Modules";
 import Grades from "./Grades";
@@ -7,8 +6,7 @@ import Assignments from "./Assignments";
 import { FaAlignJustify } from "react-icons/fa"; // Had to add this 
 import AssignmentEditor from "./Assignments/Editor";
 import Home from "./Home";
-
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
   const { pathname } = useLocation();
