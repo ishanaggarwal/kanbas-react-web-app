e417aa22bbe5f503d8d40a826d590c5463793e74
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-01 18:59:25 -0400

    Update AssignControlButtons.tsx

--- src/Kanbas/Courses/Assignments/AssignControlButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignControlButtons.tsx
@@ -1,9 +1,15 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
-export default function AssignControlButton() {
+import { FaTrash } from "react-icons/fa6";
+import AssignDelete from "./DeletePopUp";
+export default function AssignControlButton({assignmentId, deleteAssignment}: {
+  assignmentId: string; deleteAssignment: (assignmentId: string) => void; }) {
   return (
     <div className="float-end me-2">
+      <FaTrash className="text-danger me-2 mb-1" data-bs-toggle="modal" data-bs-target={`#wd-delete-assignment-${assignmentId}-dialog`}/>
       <GreenCheckmark />
       <IoEllipsisVertical className="fs-4" />
+      <AssignDelete assignmentId={assignmentId} deleteAssignment={deleteAssignment} />
+
     </div>
 );}
