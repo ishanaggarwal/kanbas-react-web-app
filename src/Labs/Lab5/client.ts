a3415120573bad0b4b6a094f3ea0b1cf6eec8da7
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-29 23:22:02 -0400

    Update client.ts

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export const fetchWelcomeMessage = async () => {
@@ -7,41 +6,46 @@ export const fetchWelcomeMessage = async () => {
   return response.data;
 };
 const ASSIGNMENT_API = `${REMOTE_SERVER}/lab5/assignment`;
-export const fetchAssignment = async () => {
-  const response = await axios.get(`${ASSIGNMENT_API}`);
-  return response.data;
+
+export const  fetchAssignment = async () => {
+    const response = await axios.get(`${ASSIGNMENT_API}`);
+    return response.data;
 };
+
 export const updateTitle = async (title: string) => {
-  const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
-  return response.data;
+    const response =  await axios.get(`${ASSIGNMENT_API}/title/${title}`);
+    return response.data;
 };
+
 const TODOS_API = `${REMOTE_SERVER}/lab5/todos`;
 export const fetchTodos = async () => {
     const response = await axios.get(TODOS_API);
     return response.data;
-  };
+};
 
 export const removeTodo = async (todo: any) => {
-const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
-return response.data;
+    const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
+    return response.data;
+};
+
+export const deleteTodo = async (todo: any) => {
+    const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+    return response.data;
 };
 
 export const createTodo = async () => {
     const response = await axios.get(`${TODOS_API}/create`);
     return response.data;
-  };
-  
-  export const postTodo = async (todo: any) => {
+};
+
+export const postTodo = async (todo: any) => {
     const response = await axios.post(`${TODOS_API}`, todo);
     return response.data;
-  };
+};
 
-  export const deleteTodo = async (todo: any) => {
-    const response = await axios.delete(`${TODOS_API}/${todo.id}`);
-    return response.data;
-  };
-  export const updateTodo = async (todo: any) => {
+export const updateTodo = async (todo: any) => {
     const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
     return response.data;
-  };
-    
+};
+
+  
