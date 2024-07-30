b05e727ad2d65811465123549439ee2da236c854
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-29 23:12:55 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Kanbas from "./Kanbas";
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import AssignmentEditor from "./Kanbas/Courses/Assignments/Editor";
 function App() {
   return (
     <HashRouter>
