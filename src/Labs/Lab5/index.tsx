dc2b2e679f006a101e8ffa5ae44711ab12a92ec0
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-29 23:22:15 -0400

    Update index.tsx

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,29 +1,38 @@
 import EnvironmentVariables from "./EnvironmentVariables";
-import HttpClient from "./HttpFile";
-import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
-import WorkingWithArrays from "./WorkingWithArrays";
-import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+import PathParameters from "./PathParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
-  return (
-    <div id="wd-lab5" className="container">
-      <h2>Lab 5</h2>
-      <div className="list-group">
-        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
-          Welcome
-        </a>
-      </div><hr />
-      <EnvironmentVariables />
-      <PathParameters />
-      <QueryParameters/>
-      <WorkingWithObjects/>
-      <WorkingWithArrays/>
-      <HttpClient/>
-      <WorkingWithObjectsAsynchronously/>
-      <WorkingWithArraysAsynchronously/>
-    </div>
-  );
+    return (
+        <div id="wd-lab5">
+            <h2>Lab 5</h2>
+            <div className="list-group">
+                <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+                Welcome 
+            </a>  
+            </div><hr />
+            <EnvironmentVariables />  
+            <PathParameters />
+            <br />
+            <QueryParameters />
+            <br />
+            <WorkingWithObjects />
+            <br />
+            <WorkingWithArrays />
+            <br />
+            <HttpClient />
+            <br />
+            <WorkingWithObjectsAsynchronously />
+            <br />
+            <WorkingWithArraysAsynchronously />
+
+
+        </div>
+    );
 }
