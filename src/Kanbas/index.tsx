5dcf6b643fd06491c2de0896c896cab500e5160a
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-30 00:24:01 -0400

    Add files via upload

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,87 @@
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+import Account from "./Account";
+import { Routes, Route, Navigate } from "react-router";
+import Courses from "./Courses";
+import "./styles.css";
+import * as client from "./Courses/client";
+import { useEffect, useState } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import ProtectedRoute from "./ProtectRoute";
+
+export default function Kanbas() {
+  const [courses, setCourses] = useState<any[]>([]);
+  const fetchCourses = async () => {
+    const courses = await client.fetchAllCourses();
+    setCourses(courses);
+  };
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  const [course, setCourse] = useState<any>({
+    _id: "0", name: "New Course: this is the course name ...", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "react.jpg", description: "New Description: this is a description for the course ..."
+  });
+
+
+  const addNewCourse = async () => {
+    const newCourse = await client.createCourse(course);
+    setCourses([ ...courses, newCourse ]);
+  };
+
+  const deleteCourse = async (courseId: any) => {
+    await client.deleteCourse(courseId)
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = async () => {
+    await client.updateCourse(course);
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
+  return (
+    <Provider store={store}>
+      <div id="wd-kanbas" className="h-100">
+      <div className="d-flex h-100">
+        <div className="d-none d-md-block bg-black">
+          <KanbasNavigation />
+        </div>
+        <div className="flex-fill p-4">
+          <Routes>
+            <Route path="/Account/*" element={<Account />} />
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Dashboard" element={
+              <ProtectedRoute>
+              <Dashboard 
+              courses={courses}
+              course={course}
+              setCourse={setCourse}
+              addNewCourse={addNewCourse}
+              deleteCourse={deleteCourse}
+              updateCourse={updateCourse}/>
+              </ProtectedRoute>
+            } />
+            {/* <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} /> */}
+            <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
+
+            <Route path="Calendar" element={<h1>Calendar</h1>} />
+            <Route path="Inbox" element={<h1>Inbox</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+
+    </Provider>
+    
+  );
+}
