eaa90f1c384ca93ef43a5846fecb9597e8141021
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-29 23:14:02 -0400

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -9,63 +9,53 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a
-          id="wd-a1"
-          href="#/Labs/Lab1"
-          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
-        >
+        <a id="wd-a1" href="#/Labs/Lab1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
           Lab 1
         </a>
       </li>
       <li className="nav-item">
-        <a
-          id="wd-a2"
-          href="#/Labs/Lab2"
-          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
-        >
+        <a id="wd-a2" href="#/Labs/Lab2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
           Lab 2
         </a>
       </li>
       <li className="nav-item">
-        <a
-          id="wd-a3"
-          href="#/Labs/Lab3"
-          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
-        >
+        <a id="wd-a3" href="#/Labs/Lab3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
           Lab 3
-        </a>
+          </a>
       </li>
       <li className="nav-item">
-        <a
-          id="wd-a4"
-          href="#/Labs/Lab4"
-          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}
-        >
+        <a id="wd-a3" href="#/Labs/Lab4"
+          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>
           Lab 4
-        </a>
+          </a>
       </li>
       <li className="nav-item">
-        <a
-          id="wd-a4"
-          href="#/Labs/Lab5"
-          className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}
-        >
+        <a id="wd-a3" href="#/Labs/Lab5"
+          className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}>
           Lab 5
-        </a>
+          </a>
       </li>
-
       <li className="nav-item">
         <a id="wd-k" href="#/Kanbas" className="nav-link">
           Kanbas
         </a>
       </li>
       <li className="nav-item">
-        <a
-          id="wd-github"
-          href="https://github.com/ishanaggarwal/kanbas-react-web-app/tree/a5"
-          className="nav-link"
-        >
-          My GitHub
+        <a id="wd-github" href="https://github.com/sdk-bens/kanbas-react-web-app" className="nav-link">
+          React App Github
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-github" href="https://github.com/sdk-bens/kanbas-node-server-app" className="nav-link">
+          HTTP Server Github
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-github" href=" https://kanbas-node-server-app-5q03.onrender.com" className="nav-link">
+        Root of the server (Render)
         </a>
       </li>
     </ul>
