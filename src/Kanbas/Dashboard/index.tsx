a791701d88d9adccb5fd9b5d5f25b223b3fadc40
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-30 21:08:07 -0400

    Update index.tsx

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -34,7 +34,7 @@ export default function Dashboard(
           <div className="row row-cols-1 row-cols-md-5 g-4">
             {courses.map((course) => (
               <div className="wd-dashboard-course col" style={{ width: "330px"}}>
-                <Link to={`/Kanbas/Courses/${course.tag}/Home`} className="text-decoration-none">
+                <Link to={`/Kanbas/Courses/${course.id}/Home`} className="text-decoration-none">
                   <div className="card rounded-3 overflow-hidden">
                     <img src={`/images/${course.image}`} height="{200}" />
                     <div className="card-body">
@@ -44,7 +44,7 @@ export default function Dashboard(
                       <p className="wd-dashboard-course-title card-text" style={{ maxHeight: 53, overflow: "hidden" }}>
                         {course.description}
                       </p>
-                      <Link to={`/Kanbas/Courses/${course.tag}/Home`} className="btn btn-primary">Go</Link>
+                      <Link to={`/Kanbas/Courses/${course.id}/Home`} className="btn btn-primary">Go</Link>
                     
                       <button onClick={(event) => {
                         event.preventDefault();
